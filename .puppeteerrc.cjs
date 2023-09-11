2e523d01d8ff99c5fe4998612eaeb93103e4ef6a
RajDevWork <rajparihar14397@gmail.com>
2023-09-12 01:36:29 +0530

    changin path-2

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const {join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(os.homedir(), '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname+'/', '.cache', 'puppeteer'),
 };
